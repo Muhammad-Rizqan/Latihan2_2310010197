3493607b1bb23fd166e0ecefad8fad8cd9c50f5f
Muhammad-Rizqan <mrizqann024@gmail.com>
2025-06-11 16:05:32 +0800

    hasilnya

--- src/latihan2/DataDiri.java
+++ src/latihan2/DataDiri.java
@@ -1,14 +1,11 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-package latihan2;
-
-/**
- *
- * @author USER
- */
 public class DataDiri {
-    
-}
+
+    String nama;
+    String npm;
+    String kelas;
+
+    public DataDiri(String nama, String npm, String kelas) {
+        this.nama = nama;
+        this.npm = npm;
+        this.kelas = kelas;
+    }
